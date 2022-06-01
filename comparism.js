ace10374351001834f5affef79b3657a1be721dc
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-01 11:10:28 +0600

    first commit

---
+++ comparism.js
@@ -0,0 +1,44 @@
+// // choto boro compare kora jay ________
+
+// console.log(5 > 6);
+// console.log(5 < 6);
+// console.log(5 == 6);
+// console.log(5 == 5);
+// console.log(12 == 10);
+// console.log(12 != 17);
+// console.log(12 !== 12);
+// console.log(12 <= 15);
+// console.log(12 >= 13);
+
+
+
+
+
+
+
+
+// var myLove = 90;
+// var youLove = 100;
+// console.log(myLove > youLove);
+
+
+
+// var myLove = 90;
+// var youLove = 100;
+// // != mane holo alada alada man value
+// console.log(myLove != youLove);
+
+
+
+//  && diye AND bujay ....
+
+console.log(studyGood && GoodPerson);
+
+
+
+//    || eita diye or bujay 
+console.log(isBank || GoodResult);
+
+
+
+
